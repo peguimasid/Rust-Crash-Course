337a201a518dcb6da735ea6f731c471df5c94f1d
Guilhermo Masid <guilhermomasid@gmail.com>
2022-07-01 17:24:45 -0300

    variables exercises part 2 done

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,6 +1,8 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
 fn main() {
-  let mut missiles = 8;
-  let ready = 2;
+  let mut missiles = STARTING_MISSILES;
+  let ready = READY_AMOUNT;
   println!("Firing {} of {} missiles", ready, missiles);
   missiles = missiles - ready;
   println!("{} missiles left", missiles);

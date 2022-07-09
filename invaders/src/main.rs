819d60ef3bbb636a746c71398e147a5983a8b1c0
Guilhermo Masid <guilhermomasid@gmail.com>
2022-07-09 18:27:27 -0300

    invaders setup capture keyboard key press

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,5 +1,11 @@
-use std::error::Error;
+use std::{error::Error, io, time::Duration};
 
+use crossterm::{
+  cursor::{Hide, Show},
+  event::{self, Event, KeyCode},
+  terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+  ExecutableCommand,
+};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -11,10 +17,31 @@ fn main() -> Result<(), Box<dyn Error>> {
   audio.add("startup", "./sounds/startup.wav");
   audio.add("win", "./sounds/win.wav");
 
-  audio.play("win");
+  audio.play("startup");
+
+  // Terminal
+  let mut stdout = io::stdout();
+  terminal::enable_raw_mode()?;
+  stdout.execute(EnterAlternateScreen)?;
+  stdout.execute(Hide)?;
+
+  // Game Loop
+
+  'gameloop: loop {
+    while event::poll(Duration::default())? {
+      if let Event::Key(key_event) = event::read()? {
+        match key_event.code {
+          KeyCode::Esc | KeyCode::Char('q') => break 'gameloop,
+          _ => (),
+        }
+      }
+    }
+  }
+
   // Cleanup
-
   audio.wait();
-
+  stdout.execute(Show)?;
+  stdout.execute(LeaveAlternateScreen)?;
+  terminal::disable_raw_mode()?;
   Ok(())
 }
